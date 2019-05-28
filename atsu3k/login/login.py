6aea0fcb1598865f28e2f6c06338aa32f661caf1
Kazuyuki ATSUMI <atsu3_k@Kazuyukis-iMac.local>
2019-05-29 08:12:59 +0900

    [update] Add configuration and error check.

--- atsu3k/login/login.py
+++ atsu3k/login/login.py
@@ -1,6 +1,16 @@
+# -*- coding: utf-8 -*-
 
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
+# Configuration
+DEBUG = True
+SECRET_KEY = 'secret key'
+USERNAME = 'test'
+PASSWORD = 'test'
+
+# Generation of Application
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 # Definition of User Information
 class User():
@@ -16,4 +26,23 @@ def form():
 # Error check
 @app.route('/login', methods=['POST'])
 def login():
-    return redirect(url_for('index.html'))
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('Succeeded in login!')
+    return render_template('login.html', error=error)
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Logged out.')
+    return redirect(url_for('login.html')) ######
+
+if __name__ == '__main__':
+    app.run()
