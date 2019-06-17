c53cc89f8bd95857cc1d014e968b6dbb325a443b
Sylphy0052 <sylphy0052@gmail.com>
2019-06-17 23:42:54 +0900

    [feat] view program for common

--- sylphy0052/crud/myapp/views/view.py
+++ sylphy0052/crud/myapp/views/view.py
@@ -2,6 +2,7 @@ from flask import request, redirect, url_for, render_template, flash, session
 from myapp import app
 from functools import wraps
 from myapp.controller.users_controller import search_user_for_login
+from datetime import timedelta
 
 # check login
 def login_required(view):
@@ -27,9 +28,12 @@ def login():
         else:
             users = search_user_for_login(request.form['username'], request.form['password'])
             if len(users) == 1:
+                session.permanent = True
+                app.permanent_session_lifetime = timedelta(minutes=30)
                 session['logged_in'] = True
+                session['user_id'] = users[0].id
                 flash('Login Success!')
-                return redirect(url_for('user.show_user_toppage', userid=users[0].id))
+                return redirect(url_for('user.show_user_toppage'))
             else:
                 flash('Invalid User Name or Password')
                 return render_template('login.html')
@@ -39,6 +43,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('Logout')
     return redirect(url_for('login'))
 
