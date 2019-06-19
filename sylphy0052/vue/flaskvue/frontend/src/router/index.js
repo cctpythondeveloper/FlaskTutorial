c2e3b93f3085a79ac4e48de5eeff694a7c99bf21
Sylphy0052 <sylphy0052@gmail.com>
2019-06-20 01:12:31 +0900

    [add] frontend

---
+++ sylphy0052/vue/flaskvue/frontend/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+  ]
+})
